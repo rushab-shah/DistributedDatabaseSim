e53f1593b98dc22076815674d2850882a7d976d1
deepalichugh <chugh@deepali.com>
2022-11-28 18:16:41 -0500

    Updated startsWith usage to read txn and created class object for TransactionManager

--- src/transaction_manager.py
+++ src/transaction_manager.py
@@ -10,25 +10,28 @@ class TransactionManager:
         print("Hello")
 
     def opProcess(self,line):
-        time = time + 1
-        if eachOperation.startswith() == "begin(":
+        # time = time + 1
+        if eachOperation.startswith("begin("):
             #initialise new transaction function
             print("Insert begin() function")
-        elif eachOperation.startswith() == "beginRO(":
+        elif eachOperation.startswith("beginRO("):
             #beginRO(T3) means T3 txn begins and is read only
             print("Insert beginRO() function")
-        elif eachOperation.startswith() == "fail(":
+        elif eachOperation.startswith("fail("):
             #insert site fail function
             print("Site fail")
-        elif eachOperation.startswith() == "R(":
+        elif eachOperation.startswith("R("):
             #Read operation. eg. R(T1,x1). Execute write() function
             print("Transaction reads x_n")
-        elif eachOperation.startswith() == "W(":
+        elif eachOperation.startswith("W("):
             #Write operation. eg. W(T2,x8,88) . Execute write() function
             print("Transaction writes value to variable x_n")
-        elif eachOperation.startswith() == "end(":
+        elif eachOperation.startswith("end("):
             #Transaction t ends. Execute end() function
             print("Transaction writes value to variable x_n")
-        elif eachOperation.startswith() == "recover(":
+        elif eachOperation.startswith("recover("):
             #eg. recover(2) => recover site 2.
             print("Recover site function to be executed")
+
+eachOperation = ""
+tm = TransactionManager()
