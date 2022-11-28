31f82361389f228885654c7d1a3736827caaab03
deepalichugh <chugh@deepali.com>
2022-11-28 18:00:21 -0500

    Updated proces function with startswith

--- src/transaction_manager.py
+++ src/transaction_manager.py
@@ -11,23 +11,21 @@ class TransactionManager:
 
     def opProcess(self,line):
         time = time + 1
-        allOperations = line.split("\n")
-        for eachOperation in allOperations:
-            if eachOperation == "begin(":
-                #initialise new transaction function
-                print("Insert begin() function")
-            elif eachOperation == "fail(":
-                #insert site fail function
-                print("Site fail")
-            elif eachOperation == "R(":
-                #Read operation. eg. R(T1,x1). Execute write() function
-                print("Transaction reads x_n")
-            elif eachOperation == "W(":
-                #Write operation. eg. W(T2,x8,88) . Execute write() function
-                print("Transaction writes value to variable x_n")
-            elif eachOperation == "end(":
-                #Transaction t ends. Execute end() function
-                print("Transaction writes value to variable x_n")
-            elif eachOperation == "recover(":
-                #eg. recover(2) => recover site 2.
-                print("Recover site function to be executed")
+        if eachOperation.startswith() == "begin(":
+            #initialise new transaction function
+            print("Insert begin() function")
+        elif eachOperation.startswith() == "fail(":
+            #insert site fail function
+            print("Site fail")
+        elif eachOperation.startswith() == "R(":
+            #Read operation. eg. R(T1,x1). Execute write() function
+            print("Transaction reads x_n")
+        elif eachOperation.startswith() == "W(":
+            #Write operation. eg. W(T2,x8,88) . Execute write() function
+            print("Transaction writes value to variable x_n")
+        elif eachOperation.startswith() == "end(":
+            #Transaction t ends. Execute end() function
+            print("Transaction writes value to variable x_n")
+        elif eachOperation.startswith() == "recover(":
+            #eg. recover(2) => recover site 2.
+            print("Recover site function to be executed")
