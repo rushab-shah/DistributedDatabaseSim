917e12dad9805e38a4bc414627205a3969003b0b
Rushab Shah <rushabshah@rushabs-air.lan>
2022-11-29 20:18:18 -0500

    Implemented release_all_locks, is read locked, is write locked

--- src/lock_mechanism.py
+++ src/lock_mechanism.py
@@ -7,7 +7,9 @@ class LockMechanism:
     # Methods might include has a lock ,get a lock, release lock, release all locks, promote lock, check if lock exists
 
     def has_lock(self, transaction_number, variable, sites):
-
+        present_all_sites = False
+        if variable%2 ==0:
+            present_all_sites = True
         return False
 
     def get_read_lock(self, transaction_number, variable):
@@ -20,14 +22,47 @@ class LockMechanism:
         self.get_write_lock(transaction_number,variable)
         return
 
-    def release_lock(self,transaction_number, variable):
+    def release_lock(self,transaction_number, variable, sites):
         return True
 
-    def release_all_locks(self, transaction_number):
+    def release_all_locks(self, transaction_number, sites):
+        locks = []
+        for site in sites:
+            locks = [lock for lock in site.lock_table if lock.transaction==transaction_number]
+            for lock in locks:
+                site.lock_table.remove(lock)
         return
     
     def is_read_locked(self, variable, sites):
-        return
+        present_all_sites = False
+        if variable%2 ==0:
+            present_all_sites = True
+        if present_all_sites:
+            lock = []
+            for site in sites:
+                lock = [lock for lock in site.lock_table if lock.variable==variable and lock.type==1]
+                if(len(lock)>0):
+                    return True
+        else:
+            site_number = 1 + variable%10
+            lock = [lock for lock in sites[site_number-1].lock_table if lock.variable==variable and lock.type==1]
+            if(len(lock)>0):
+                return True
+        return False
     
     def is_write_locked(self, variable, sites):
-        return
+        present_all_sites = False
+        if variable%2 ==0:
+            present_all_sites = True
+        if present_all_sites:
+            lock = []
+            for site in sites:
+                lock = [lock for lock in site.lock_table if lock.variable==variable and lock.type==2]
+                if(len(lock)>0):
+                    return True
+        else:
+            site_number = 1 + variable%10
+            lock = [lock for lock in sites[site_number-1].lock_table if lock.variable==variable and lock.type==2]
+            if(len(lock)>0):
+                return True
+        return False
